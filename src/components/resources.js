b3c04ba9e680a314864defbd371635b47747eadd
Cory Hoban <hobancor@gmail.com>
2018-01-29 11:34:25 -0500

    Add Resources Route

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Recipe
+      <ul>
+        <li>1 Cup Sugar</li>
+        <li>1 Cup Termuric</li>
+        <li>1 Cup Garam Masala</li>
+      </ul>
+    </div>
+  );
+};
